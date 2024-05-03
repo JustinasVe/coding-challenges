9621eafad48764c37018972e4b884d74241a931e
unknown <justinasve@gmail.com>
2024-05-03 21:07:25 +0300

    solved even-or-odd task

--- coderbyte/script.js
+++ coderbyte/script.js
@@ -1,2 +1,3 @@
 import { arrayAdditionI } from "./easy/array-addition-i.js";
 import { points } from "../codewars/8kyu/total-amount-of-points.js";
+import { evenOrOdd } from "../codewars/8kyu/even-or-odd.js";
