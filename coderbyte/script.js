f01c47b73f063769dd874edffa93392bf18142d3
unknown <justinasve@gmail.com>
2024-05-06 22:50:16 +0300

    solved return-negative task using if else or ternary op

--- coderbyte/script.js
+++ coderbyte/script.js
@@ -3,3 +3,4 @@ import { points } from "../codewars/8kyu/total-amount-of-points.js";
 import { evenOrOdd } from "../codewars/8kyu/even-or-odd.js";
 import { reversedString } from "../codewars/8kyu/reversed-string.js";
 import { countSheeps } from "../codewars/8kyu/counting-sheep.js";
+import { makeNegative } from "../codewars/8kyu/return-negative.js";
